4fc28a0a684c5e965a04d3a735af63f7812eb6c7
Bohaddin MD <95641388+BohaddinMD@users.noreply.github.com>
2026-05-25 15:37:51 +0530

    n

--- app.py
+++ app.py
@@ -1,22 +1,53 @@
-from flask import Flask, request
+from flask import Flask, request, Response
+import threading
+import time
 
 app = Flask(__name__)
 
 latest_frame = None
+frame_lock = threading.Lock()
+
 
 @app.route("/")
 def home():
     return "Flask server running"
 
-@app.route("/live")
-def live():
-    return "Waiting for video"
 
 @app.route("/upload", methods=["POST"])
 def upload():
+
     global latest_frame
-    latest_frame = request.data
+
+    with frame_lock:
+        latest_frame = request.data
+
     return "Frame received"
 
+
+@app.route("/live")
+def live():
+
+    def generate():
+        while True:
+
+            with frame_lock:
+                frame = latest_frame
+
+            if frame:
+                yield (
+                    b'--frame\r\n'
+                    b'Content-Type: image/jpeg\r\n\r\n'
+                    + frame +
+                    b'\r\n'
+                )
+
+            time.sleep(0.05)
+
+    return Response(
+        generate(),
+        mimetype='multipart/x-mixed-replace; boundary=frame'
+    )
+
+
 if __name__ == "__main__":
-    app.run(host="0.0.0.0")
+    app.run(host="0.0.0.0", port=5000)
