51af0cfcd51ef7f5973d4e0aa9e43684b842f22d
Bohaddin MD <95641388+BohaddinMD@users.noreply.github.com>
2026-05-25 14:41:36 +0530

    changed_nes

--- app.py
+++ app.py
@@ -1,14 +1,23 @@
-from flask import Flask
+from flask import Flask, Response
+import requests
 
 app = Flask(__name__)
 
+# Replace with your ESP32-CAM local stream
+ESP32_URL = "http://192.168.1.100:81/stream"
+
 @app.route("/")
 def home():
-    return "Flask server working"
+    return "Flask server running"
 
 @app.route("/live")
-def live():
-    return "MJPEG stream will come here"
+def video_feed():
+    r = requests.get(ESP32_URL, stream=True)
+
+    return Response(
+        r.iter_content(chunk_size=1024),
+        content_type=r.headers["Content-Type"]
+    )
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
